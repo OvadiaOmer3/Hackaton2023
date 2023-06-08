7b8456f17e0627d01b3e530d4753bb84eda58138
Your Name <fainzilber@mail.tau.ac.il>
2023-06-08 20:49:37 +0300

    20:50 version

--- Route_aggregation.py
+++ Route_aggregation.py
@@ -1,8 +1,9 @@
 # Pseudo code for route aggregation
 # Takes the route data from RouteModel and aggregates to user presentable data
 
-def token_calculator(route_object, user_rank):
+def token_calculator(route_object, user):
     tokens = 0
+    user_rank = user.rank
     
     if route_object.mode == "car":
         tokens = 1*user_rank
@@ -50,27 +51,29 @@ def eco_relative(route_object): #This function calculates the eco relative to ca
     eco_relative = (co2_emissions_calculator(route_object) / car_co2_emmissions)*100
     return round(eco_relative)
 
-def rank_change_calculator(route_object): # This function will calculate rank change for the route
-    rank_change = 0
+def score_calculator(route_object, user): # This function will calculate rank change for the route
+    import numpy as np
+    last_eco_relative_routes = user.eco_relative_scores[-30:]
+    last_eco_relative_routes.append(eco_relative(route_object))
+    
+    return(np.mean(100 - last_eco_relative_routes))
 
-    return(rank_change)
 
-
-def presentable_data(route_object, user_rank):
+def presentable_data(route_object, user):
     mode = route_object.mode
     arrival_time = route_object.arrival_time
     total_time = route_object.duration
     walking_distance = route_object.walking_distance
     walking_duration = route_object.walking_duration
     cost = route_object.fare
-    tokens = token_calculator(route_object, user_rank)   # This will be a function that calculates tokens reward for the route
+    tokens = token_calculator(route_object, user)   # This will be a function that calculates tokens reward for the route
     health_benefit = health_benefit_calculator(route_object)   # This will be a function that calculates health benefit for the route
     co2_emissions = co2_emissions_calculator(route_object)   # This will be a function that calculates co2 emissions for the route
     eco_relative_to_car = eco_relative(route_object)
-    rank_change = rank_change_calculator(route_object)   # This will be a function that calculates rank change for the 
+    score = score_calculator(route_object)   # This will be a function that calculates rank change for the 
 
     out_dict = {"mode": mode , "arrival_time": arrival_time, "total_time": total_time, "walking_distance": walking_distance, 
                 "walking_duration": walking_duration, "cost": cost, "tokens": tokens, "health_benefit": health_benefit, 
-                "co2_emissions": co2_emissions, "eco_relative_to_car": eco_relative_to_car , "rank_change": rank_change}
+                "co2_emissions": co2_emissions, "eco_relative_to_car": eco_relative_to_car , "score_change": score}
     
     return(out_dict)
