5e8e6c9a867cc34afb82ba86f756723c13dbbb55
Your Name <fainzilber@mail.tau.ac.il>
2023-06-08 19:20:59 +0300

    almost final

--- Route_aggregation.py
+++ Route_aggregation.py
@@ -3,13 +3,19 @@
 
 def token_calculator(route_object, user_rank):
     tokens = 0
-
+    
     if route_object.mode == "car":
-        tokens = 1
+        tokens = 1*user_rank
     elif route_object.mode == "taxi":
-        tokens = 2
+        tokens = 2*user_rank
     else:
-        # finish later!!!!
+        walking_km = route_object.walking_distance/1000
+        bus_km = route_object.bus_distance/1000
+        train_km = route_object.train_distance/1000
+        cycling_km = route_object.cycling_distance/1000
+        e_scooter_km = route_object.e_scooter_distance/1000
+        tmp_tokens = 6*(walking_km+cycling_km) + 5*e_scooter_km + 4*train_km + 3*bus_km
+        tokens = tmp_tokens*user_rank
 
     return(tokens)
 
@@ -24,12 +30,25 @@ def health_benefit_calculator(route_object): # This function will calculate calo
     else:
         health_benefit = 0
 
-    return(health_benefit)
+        return(health_benefit)
 
-def co2_emissions_calculator(route_object): # This function will calculate co2 emissions for the route
-    co2_emissions = 0
+def co2_emissions_calculator(route_object): #This function calculates the CO2 emissions of a given route (various modes of transport)
+    emissions = 0
+    bus_distance = route_object.bus_distance
+    train_distance = route_object.train_distance
+    car_distance = route_object.car_distance
+    taxi_distance = route_object.taxi_distance
+    e_scooter_distance = route_object.e_scooter_distance
+    emissions = (bus_distance * 30) + (train_distance * 60) + (car_distance * 200) + (taxi_distance * 180) + (e_scooter_distance * 100)
+    emissions = emissions / 1000
+    #Emissions are calculated in kg per km, need to validate these values
 
-    return(co2_emissions)
+    return(emissions)
+
+def eco_relative(route_object): #This function calculates the eco relative to car
+    car_co2_emmissions = co2_emissions_calculator(car_route_object) # list indexing for the car route object
+    eco_relative = (co2_emissions_calculator(route_object) / car_co2_emmissions)*100
+    return round(eco_relative)
 
 def rank_change_calculator(route_object): # This function will calculate rank change for the route
     rank_change = 0
@@ -37,7 +56,7 @@ def rank_change_calculator(route_object): # This function will calculate rank ch
     return(rank_change)
 
 
-def presentable_data(route_object):
+def presentable_data(route_object, user_rank):
     mode = route_object.mode
     arrival_time = route_object.arrival_time
     total_time = route_object.duration
@@ -47,10 +66,11 @@ def presentable_data(route_object):
     tokens = token_calculator(route_object, user_rank)   # This will be a function that calculates tokens reward for the route
     health_benefit = health_benefit_calculator(route_object)   # This will be a function that calculates health benefit for the route
     co2_emissions = co2_emissions_calculator(route_object)   # This will be a function that calculates co2 emissions for the route
+    eco_relative_to_car = eco_relative(route_object)
     rank_change = rank_change_calculator(route_object)   # This will be a function that calculates rank change for the 
 
     out_dict = {"mode": mode , "arrival_time": arrival_time, "total_time": total_time, "walking_distance": walking_distance, 
                 "walking_duration": walking_duration, "cost": cost, "tokens": tokens, "health_benefit": health_benefit, 
-                "co2_emissions": co2_emissions, "rank_change": rank_change}
+                "co2_emissions": co2_emissions, "eco_relative_to_car": eco_relative_to_car , "rank_change": rank_change}
     
     return(out_dict)
